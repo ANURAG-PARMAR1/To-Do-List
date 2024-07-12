782598c4d51f9c5b194c97330086f7fd61e303a2
Anurag <anurag.p00000@gmail.com>
2024-07-12 15:47:42 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Form from './components/Form';
+import List from './components/List';
+import ClearButton from './components/ClearButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getLocalItems(){
+  let list = localStorage.getItem('listItems')
+  if (list) return JSON.parse(list)
+  else return [];
 }
 
-export default App;
+export default function App() {
+
+  const [items, setItems] = useState(getLocalItems());
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleRemoveItem(task) {
+    setItems((items) => items.filter(item => item.task !== task))
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm('Are you sure you want to clear the list')
+    if (confirmed) setItems([]);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('listItems', JSON.stringify(items));
+  },[items])  
+
+  return (
+    <main className="flex flex-col items-center overscroll-none overflow-y-visible">
+      <h1 className="text-white font-bold text-3xl p-5 text-center md:text-5xl md:p-10">
+        To-Do List
+      </h1>
+      <Form onAddItems={handleAddItem} className=""></Form>
+      <List items={items} onDeleteItems={handleRemoveItem}></List>
+      <div>
+        {items.length !== 0 &&
+            <ClearButton onClearList={handleClearList}></ClearButton>
+        }
+      </div>
+    </main>
+  );
+}
+//rafc
